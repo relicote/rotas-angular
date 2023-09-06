0cd287457940653f4b622ca64a4f95ad3232622c
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-09-06 11:09:44 -0300

    att menu-component

--- rotas/src/app/app-routing.module.ts
+++ rotas/src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { CardComponent } from './pages/portfolio/card/card.component';
 
 const routes: Routes = [
   {path: '', component: TitleComponent, pathMatch:'full'},
-  {path:'portfolio', component: CardComponent, pathMatch:'prefix'}
+  {path:'portfolio', component: CardComponent},
+  {path:'**', redirectTo:''}
 ];
 
 @NgModule({
